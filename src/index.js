4c81fa626248f17e1f99d2332934cf771dc6831f
Juan Tomás Araque Martinez <63049937+JTAraque@users.noreply.github.com>
2021-03-26 19:53:43 +0100

    Posts addition and editing

--- src/index.js
+++ src/index.js
@@ -2,20 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter, Route } from 'react-router-dom';
 
-import 'bootstrap/dist/css/bootstrap.css';
-
-import './index.css';
 import reportWebVitals from './reportWebVitals';
 
-// import Home from './components/Home';
+import 'bootstrap/dist/css/bootstrap.css';
+
+import Home from './components/Home';
 import App from './components/App';
 
 ReactDOM.render(<HashRouter>
-  <div>
-    <Route exact path="/" component={App} />
-    {/* <Route path="/home" component={Home} /> */}
-  </div>
-</HashRouter >, document.getElementById('root'));
+      <div>
+        <Route exact path="/" component={App} />
+        <Route path="/home" component={Home} />
+      </div>
+   </HashRouter >, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
